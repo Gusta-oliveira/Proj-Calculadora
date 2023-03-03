5d46074f7eaf18ab354d8c84d5df0aada01e4aa9
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-03 15:53:02 -0300

    Foi adicionado um menu com funcao ao programa

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,25 +1,57 @@
-﻿internal class Program
+﻿double num1, num2;
+int op = 0;
+void Menu()
 {
-    private static void Main(string[] args)
+    Console.Clear();
+    Console.WriteLine("1 - Somar");
+    Console.WriteLine("2 - Subtrair");
+    Console.WriteLine("3 - Multiplicar");
+    Console.WriteLine("4 - Dividir");
+    Console.WriteLine("0 - Sair");
+    Console.Write("Escolha uma opção:  ");
+    op = int.Parse(Console.ReadLine());
+    switch (op)
     {
-        float dig_um, dig_dois;
-        Console.WriteLine("Informe primeiro digito: ");
-        dig_um = float.Parse(Console.ReadLine());
-        Console.WriteLine("Informe segundo digito: ");
-        dig_dois = float.Parse(Console.ReadLine());
 
-        Console.WriteLine("Soma: " + dig_um + " + " + dig_dois + " = " + (dig_um + dig_dois));
-        Console.WriteLine("Subtração: " + dig_um + " - " + dig_dois + " = " + (dig_um - dig_dois));
-        Console.WriteLine("Multiplicação: " + dig_um + " * " + dig_dois + " = " + (dig_um * dig_dois));
+        default:
+            Console.WriteLine("Opção inválida!!");
+            Console.ReadLine();
+            break;
+        case 1: 
+            Console.WriteLine($"A soma dos números é: {num1 + num2}");
+            Console.ReadLine();
+            break;
+        case 2:
+            Console.WriteLine($"A subtração dos números é: {num1 - num2}");
+            Console.ReadLine();
+            break;
 
-        if (dig_dois == 0)
-        {
-            Console.WriteLine("Não existe divisão por ZERO!");
-        }
-        else
-        {
-            Console.WriteLine("Divisão: " + dig_um + " / " + dig_dois + " = " + (dig_um / dig_dois));
-        }
-        Console.ReadLine();
+        case 3:
+            Console.WriteLine($"A multiplicação dos números é: {num1 * num2}");
+            Console.ReadLine();
+            break;
+
+        case 4:
+            Console.WriteLine($"A divisão dos números é: {num1 / num2}");
+            Console.ReadLine();
+            break;
+
+        case 5:
+            Console.WriteLine($"Opção sair selecionada");
+            Console.ReadLine();
+            break;
+
+
     }
+}
+
+Console.Write("Informe o primeiro número: ");
+num1 = double.Parse(Console.ReadLine());
+Console.Write("Informe o segundo número: ");
+num2 = double.Parse(Console.ReadLine());
+
+
+while(op != 5)
+{
+    Menu();
 }
